257364e1e039b814c8ca6aaf882614fd2ca0d02c
Arhangel69 <33459102+Arhangel69@users.noreply.github.com>
2021-05-23 23:41:39 +0300

    Update hseAD_script.ps1

--- hseAD_script.ps1
+++ hseAD_script.ps1
@@ -1,11 +1,51 @@
 echo 'Start HSE-AD autoconnect'
-if (((Get-NetIPConfiguration).NetProfile.Name.contains('HSE-AD')) -and ((Test-NetConnection).PingSucceeded -eq $false))
+echo 'Please wait...'
+
+$waitingSetupAttempts = 15
+$netIPConfiguration = (Get-NetIPConfiguration)
+
+if ($netIPConfiguration -eq $null)
+{
+	echo 'Waiting for setup NetIPConfiguration...'
+	while (($netIPConfiguration -eq $null) -and ($waitingSetupAttempts -ne 0))
+	{
+		Start-Sleep -s 1
+		$waitingSetupAttempts -= 1
+		$netIPConfiguration = (Get-NetIPConfiguration)
+	}
+
+	if ($netIPConfiguration -eq $null)
+	{
+		echo 'Waiting too long'
+		Return
+	}
+}
+
+if ($netIPConfiguration.NetProfile.Name -eq $null)
+{
+	echo 'Waiting for resolving NetProfile.Name...'
+	$waitingSetupAttempts = 15
+	while (($netIPConfiguration.NetProfile.Name -eq $null) -and ($waitingSetupAttempts -ne 0))
+	{
+		Start-Sleep -s 1
+		$waitingSetupAttempts -= 1
+		$netIPConfiguration = (Get-NetIPConfiguration)
+	}
+
+	if ($netIPConfiguration.NetProfile.Name -eq $null)
+	{
+		echo 'Waiting too long'
+		Return
+	}
+}
+
+if (($netIPConfiguration.NetProfile.Name.contains('HSE-AD')) -and ((Test-NetConnection).PingSucceeded -eq $false))
 {
 	echo 'Waiting for autoconnect'
 	Start-Sleep -Second 2
 	if ((Test-NetConnection).PingSucceeded -eq $false)
 	{
 		echo 'Autoconnect not available. Force connect'
-		."<path-to-chrome.exe>" https://wifi.mts.ru/redirect.php
+		."C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" https://wifi.mts.ru/redirect.php
 	}
 }
